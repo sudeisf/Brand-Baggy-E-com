a6605830da1a70bbfc6a4e9a96895089f7a83eab
sudeis <sudeisfed@gmail.com>
2025-04-18 23:35:43 +0300

    Add signal loading in CartConfig and fix whitespace in product URLs

--- product/urls.py
+++ product/urls.py
@@ -12,7 +12,6 @@ urlpatterns =[
     path('product-list/', ProductListView.as_view() , name="products-list"),
     path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('products/<int:product_id>/reviews/', ProductReviewListCreateView.as_view(), name='product-reviews'),
-     path('favorites/add/<int:product_id>/', AddFavoriteProductView.as_view(), name='add-favorite'),
+    path('favorites/add/<int:product_id>/', AddFavoriteProductView.as_view(), name='add-favorite'),
     path('favorites/remove/<int:product_id>/', RemoveFavouriteProductView.as_view(), name='remove-favorite'),
-
 ]   
