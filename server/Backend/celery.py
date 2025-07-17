9d8f42360ae09e355789ec6276b93664f6148849
sudeis <sudeisfed@gmail.com>
2025-07-17 15:49:27 +0300

    fix nameing isuue

--- server/Backend/celery.py
+++ server/Backend/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_django_server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
 app = Celery('first_django_server')
 app.config_from_object('django.conf:settings', namespace='CELERY')
