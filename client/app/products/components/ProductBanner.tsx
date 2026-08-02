632f7c8f181cf8d51a2da20b391648b924dc52c8
Sudeis <sudeisfed@gmail.com>
2026-08-02 14:25:29 +0300

    style: update ProductBanner typography, fonts, and text content for improved readability

--- client/app/products/components/ProductBanner.tsx
+++ client/app/products/components/ProductBanner.tsx
@@ -3,17 +3,16 @@
 import { useState } from "react";
 import Image from "next/image";
 
-export default function ProductBanner(){
-    return(
+export default function ProductBanner() {
+    return (
         <div className="flex flex-col lg:flex-row items-center justify-between w-full h-[400px]   rounded-md bg-[#7f72a037] overflow-hidden">
-                        
-            <div className="flex flex-col  w-[400px] md:w-[600px] mt-10 lg:mt-0  mb-10 lg:mb-0">
-                <h1 className="text-3xl md:text-5xl text-center lg:text-left py-6  ml-0 lg:ml-20 px-2 font-mrs-saint-delafield font-bold text-[#331d67]  ">Styele of the Generation</h1>
-                <div className="bg-white w-[400px] md:w-[600px]  mx-auto lg:mx-0 lg:w-[450px] xl:w-[600px] rounded-md border shadow-sm space-y-2 text-[#331d67] ml-0  lg:ml-10 xl:ml-20 h-[300px] md:h-[200px] px-10 py-10 z-10">
-                    <p className="text-md text-[#331d676d] font-teko  font-bold">Style items</p>
-                    <p className="text-5xl lg:text-4xl xl:text-5xl font-medium font-teko">A Style with out this items is not complete</p>
-                </div>
-            </div>
+         <div className="flex flex-col w-[400px] md:w-[600px] mt-10 lg:mt-0 mb-10 lg:mb-0">
+    <h1 className="text-3xl md:text-5xl text-center lg:text-left py-6 ml-0 lg:ml-20 px-2 font-serif font-bold text-[#331d67]">Style of the Generation</h1>
+    <div className="bg-white w-[400px] md:w-[600px] mx-auto lg:mx-0 lg:w-[450px] xl:w-[600px] rounded-md border shadow-sm space-y-2 text-[#331d67] ml-0 lg:ml-10 xl:ml-20 h-[300px] md:h-[200px] px-10 py-10 z-10">
+        <p className="text-md text-[#331d676d] font-sans font-bold uppercase tracking-wide">Style items</p>
+        <p className="text-5xl lg:text-4xl xl:text-5xl font-medium font-sans">A Style without these items is not complete</p>
+    </div>
+</div>
                 <div className=" w-1/2 space-x-3 hidden lg:flex h-full justify-end z-10 ">
                     <Image src="/assets/products/products-banner.jpg"
                         alt="product banner" width={150} height={100}
@@ -25,10 +24,10 @@ export default function ProductBanner(){
                     />
                     <Image src="/assets/products/products-banner-3.jpg"
                         alt="product banner" width={150} height={150}
-                        className=" object-cover object-center h-full "
+                        className=" object-cover object-center h-full " 
                     />
                     
                 </div>
-            </div>
+            </div >
     );
 }
